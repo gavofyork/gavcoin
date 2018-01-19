9231406d17f310331f6e919c8c777bec9052608c
Jaco Greeff <jacogr@gmail.com>
2018-01-19 16:48:19 +0100

    Create own @parity/api instance

--- src/parity.js
+++ src/parity.js
@@ -1,6 +1,14 @@
 // Copyright 2016 Gavin Wood
 
-const { api } = window.parity;
+const Api = require('@parity/api');
+
+let api;
+
+if (!window.ethereum) {
+  api = window.parity.api;
+} else {
+  api = new Api(window.ethereum);
+}
 
 export {
   api
